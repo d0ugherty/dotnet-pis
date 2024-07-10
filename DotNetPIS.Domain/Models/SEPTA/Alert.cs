d9c5daea57644f28942a2f2d9bd51f2d7fce4384
d0ugherty <tdougherty16@gmail.com>
2024-07-10 10:13:04 -0400

    nullable properties

--- DotNetPIS.Domain/Models/SEPTA/Alert.cs
+++ DotNetPIS.Domain/Models/SEPTA/Alert.cs
@@ -7,18 +7,18 @@ public class Alert
     public string? RouteName { get; set; }
     public string? Sequence { get; set; }
     public string? Mode { get; set; }
-    public bool IsAdvisory { get; set; }
-    public bool IsDetour { get; set; }
-    public bool IsAlert { get; set; }
-    public bool IsSuspended { get; set; }
-    public bool IsElevator { get; set; }
-    public bool IsStrike { get; set; }
-    public bool IsModifiedService { get; set; }
-    public bool IsDelays { get; set; }
-    public bool IsDiversion { get; set; }
-    public bool IsDetourAlert { get; set; }
-    public bool IsSnow { get; set; }
-    public DateTime LastUpdated { get; set; }
+    public bool? IsAdvisory { get; set; }
+    public bool? IsDetour { get; set; }
+    public bool? IsAlert { get; set; }
+    public bool? IsSuspended { get; set; }
+    public bool? IsElevator { get; set; }
+    public bool? IsStrike { get; set; }
+    public bool? IsModifiedService { get; set; }
+    public bool? IsDelays { get; set; }
+    public bool? IsDiversion { get; set; }
+    public bool? IsDetourAlert { get; set; }
+    public bool? IsSnow { get; set; }
+    public DateTime? LastUpdated { get; set; }
     public string? Description { get; set; }
     public string? AlertText { get; set; }
     public string? Advisory { get; set; }
